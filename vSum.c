67ba421b38482189d03a8c7744c7740451490fdc
Dário Fernandes <dario@pop-os.localdomain>
2022-03-18 18:55:47 +0000

    vSum_1.0

---
+++ vSum.c
@@ -0,0 +1,22 @@
+/*
+    Auth: Dário Fernandes
+    NMec: 93135
+    SETR
+
+    This function will compute the sum of all values in the vector
+*/
+
+#include <stdio.h>
+#include "funçoes.h"   // library with all the functions
+
+int vSum(int n, int* vect)
+{
+    int sum = 0;
+
+    for(int i=0; i<=n, i++)
+    {
+        sum = sum + vect[i];  // faz a soma de todos os elementos em cada iteração
+    }
+
+    return sum;
+}
