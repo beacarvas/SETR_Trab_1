b97049d44bbd22241a20c8db0bed755e27770718
Beatriz Carvas <beacarvas>
2022-03-20 03:15:26 +0000

    funcao vinit funcional

--- vInit.c
+++ vInit.c
@@ -3,13 +3,27 @@
 #include <stdio.h>
 #include "funcoes.h"
 
-int vInit(int N,int *vect)
+void vInit(int N,int *vect)
 {
     for (int i=0; i < N; i++ )
     {
         vect[i] = i +1;
-
+        
     }
 
 }
 
+/* para testar a função 
+int main(){
+    int N=5;
+    int vect[N];
+    vInit(N,vect);
+
+    for (int i=0; i < N; i++ )
+    {
+        printf("%d\n",vect[i]);
+    }
+    return 0;
+}
+
+*/
