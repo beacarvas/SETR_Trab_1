87cd186fe8cd9e17a642a8db997a6a3e7da23fb7
Guilherme Cajeira <gcajeira@ua.pt>
2022-03-20 18:37:04 +0000

    aa

--- vAvg.c
+++ vAvg.c
@@ -11,12 +11,7 @@ float vAvg(int n,int *vect)
     int soma = 0;
     float media = 0;
 
-    for(int i=0;i<n;i++)
-    {
-        soma = soma + vect[i];
-    }
-
-    //soma = vSum(n,vect);
+    soma = vSum(n,vect);
 
     media = soma / n;
 
--- vSum.c
+++ vSum.c
@@ -13,7 +13,7 @@ int vSum(int n, int* vect)
 {
     int sum = 0;
 
-    for(int i=0; i<=n; i++)
+    for(int i=0; i<n; i++)
     {
         sum = sum + vect[i];  // faz a soma de todos os elementos em cada iteração
     }
