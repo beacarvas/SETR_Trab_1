e1923c1ea8e807b88194f498e29b5de83c573d9b
Guilherme Cajeira <gcajeira@ua.pt>
2022-03-22 19:03:50 +0000

    aa

--- Assignment_1/test_app.c
+++ Assignment_1/test_app.c
@@ -42,38 +42,44 @@ int main(void)
 	printf("Qual o tamanho do módulo? ");
 	scanf("%d",&N);
 
-	int init = MySAGInit(N,stream);
+	bool init = MySAGInit(N,stream);
 
-	MySAGInsert(N,stream);
-	printf("Stream = ");
-	for(int i=0;i<N-1;i++)
+	if (init == TRUE)
 	{
-		printf("%d , ",stream[i]);
+		MySAGInsert(N,stream);
+		printf("Stream = ");
+		for(int i=0;i<N-1;i++)
+		{
+			printf("%d , ",stream[i]);
+		}
+		printf("%d",stream[N-1]);
+		printf("\n");
+
+		// ------------------- MÁXIMO ----------------------
+		int max = MySAGMax(N,stream);
+		printf("O valor máximo do módulo é %d \n",max);
+
+		// ------------------- MÍNIMO ----------------------
+		int min = MySAGMin(N,stream);
+		printf("O valor mínimo do módulo é %d \n",min);
+
+		// ------------------- MÉDIA ----------------------
+		int avg = MySAGAvg(N,stream);
+		printf("A média do módulo é %d \n",avg);
+
+		// ----------------- FREQUÊNCIA -------------------
+		int x = 0;
+		printf("Valor a ver o número de vezes que aparece: ");
+		scanf("%d",&x);
+		int freq = MySAGFreq(N,stream,x);
+		if (freq == 0)
+			printf("Não há nenhum número no módulo igual a %d \n",x);
+		else 
+			printf("O número %d existe %d vezes no módulo \n",x,freq);
 	}
-	printf("%d",stream[N-1]);
-	printf("\n");
+	else
+		printf("ERROR- MAXSIZE foi ultrapassado \n");
 
-	// ------------------- MÁXIMO ----------------------
-	int max = MySAGMax(N,stream);
-	printf("O valor máximo do módulo é %d \n",max);
-
-	// ------------------- MÍNIMO ----------------------
-	int min = MySAGMin(N,stream);
-	printf("O valor mínimo do módulo é %d \n",min);
-
-	// ------------------- MÉDIA ----------------------
-	int avg = MySAGAvg(N,stream);
-	printf("A média do módulo é %d \n",avg);
-
-	// ----------------- FREQUÊNCIA -------------------
-	int x = 0;
-	printf("Valor a ver o número de vezes que aparece: ");
-	scanf("%d",&x);
-	int freq = MySAGFreq(N,stream,x);
-	if (freq == 0)
-		printf("Não há nenhum número no módulo igual a %d \n",x);
-	else 
-		printf("O número %d existe %d vezes no módulo \n",x,freq);
 
 	return 0;
 } 
