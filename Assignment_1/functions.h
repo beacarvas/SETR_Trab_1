5a01fc7169555e1fa6361f8d50a93aa63bc56651
Guilherme Cajeira <gcajeira@ua.pt>
2022-03-22 10:41:51 +0000

    Primeira versão main

--- Assignment_1/functions.h
+++ Assignment_1/functions.h
@@ -3,6 +3,11 @@
 #ifndef _FUNCTIONS_H
 #define _FUNCTIONS_H
 
-
+void MySAGInit();
+void MySAGInsert();
+int MySAGMax(int N,int *stream);
+int MySAGMin(int N,int *stream);
+int MySAGAvg(int N,int *stream);
+int MySAGFreq(int N,int *stream,int x);
 
 #endif
--- Assignment_1/test_app.c
+++ Assignment_1/test_app.c
@@ -7,7 +7,31 @@
 
 int main(void)
 {
-	
+	int N = 0;
+
+
+
+	// ------------------- MÁXIMO ----------------------
+	int max = MySAGMax(N,stream);
+	printf("O valor máximo do módulo é %d \n",max);
+
+	// ------------------- MÍNIMO ----------------------
+	int min = MySAGMin(N,stream);
+	printf("O valor mínimo do módulo é %d \n",min);
+
+	// ------------------- MÉDIA ----------------------
+	int avg = MySAGAvg(N,stream);
+	printf("A média do módulo é %d \n",avg);
+
+	// ----------------- FREQUÊNCIA -------------------
+	int x = 0;
+	printf("Valor a ver o número de vezes que aparece: ");
+	scanf("%d",&x);
+	int freq = MySAGFreq(N,stream,x);
+	if (freq == 0)
+		printf("Não há nenhum número no módulo igual a %d",x);
+	else 
+		printf("O número %d existe %d vezes no módulo",x,freq);
 
     
 	return 0;
