9b4c7d183c77beead3d46bdffb3aaf33323dfd57
Dário Fernandes <dario@pop-os.localdomain>
2022-03-22 18:34:27 +0000

    Update Functions

--- Assignment_1/functions.h
+++ Assignment_1/functions.h
@@ -17,15 +17,16 @@
 #ifndef _FUNCTIONS_H
 #define _FUNCTIONS_H
 
-#define MAXSIZE 100;  // Tamanho máximo do módolo admitido
+#define MAXSIZE 100  // Tamanho máximo do módolo admitido
 
 /**
- * @brief Criação e inicialização do módulo **stream** com números naturais
+ * @brief Criação e inicialização do módulo **stream**
  * 
- * A função vInit preenche um vetor de numeros naturais, com um tamanho escolhido pelo o utilizador.
+ * A função MySAGInit cria e inicializa o módulo, com um tamanho escolhido pelo o utilizador. \n
+ * O tamanho do módulo escolhido não deve exceder o limite máximo imposto **MAXSIZE**.
  * 
- * @param n Representa o tamanho do vetor vect
- * @param vect Representa o vetor 
+ * @param N Representa o tamanho do módulo
+ * @param stream Representa o módulo
  * 
  * @verbatim
    Autor:
@@ -34,11 +35,86 @@
  */
     void MySAGInit(int N,int *stream);
 
+/**
+ * @brief Preenchimento do módulo **stream** com números naturais
+ * 
+ * A função MySAGInsert é responsável por preencher o módulo criado com numeros naturais.
+ * 
+ * @param N Representa o tamanho do módulo
+ * @param stream Representa o módulo
+ * 
+ * @verbatim
+   Autor:
+    * Beatriz Carvas, nº93209
+    * Email: beatrizcarvas@ua.pt @endverbatim
+ */
+    void MySAGInsert(int N,int *stream);
 
-void MySAGInsert(int N,int *stream);
-int MySAGMax(int N,int *stream);
-int MySAGMin(int N,int *stream);
-int MySAGAvg(int N,int *stream);
+/**
+ * @brief Devolve o valor máximo do módulo **stream**
+ * 
+ * A função MySAGMax percorre todo o módulo e identifica o valor máximo presente.
+ * 
+ * @param N Representa o tamanho do módulo
+ * @param stream Representa o módulo
+ * @return Retorna o o valor máximo presente no módulo
+ * 
+ * @verbatim
+   Autor:
+    * Dário Fernandes, nº93135
+    * Email: d.fernandes15@ua.pt @endverbatim
+ */
+    int MySAGMax(int N,int *stream);
+
+/**
+ * @brief Devolve o valor mínimo do módulo **stream**
+ * 
+ * A função MySAGMax percorre todo o módulo e identifica o valor mínimo presente.
+ * 
+ * @param N Representa o tamanho do módulo
+ * @param stream Representa o módulo
+ * @return Retorna o o valor mínimo presente no módulo
+ * 
+ * @verbatim
+   Autor:
+    * Dário Fernandes, nº93135
+    * Email: d.fernandes15@ua.pt @endverbatim
+ */
+    int MySAGMin(int N,int *stream);
+
+/**
+ * @brief Calcula a média do módulo **stream**
+ * 
+ * A função MySAGAvg recebe o módulo e o valor do seu tamanho e calcula a média
+ * de todos os valores, somando-os um a um e no fim divide pelo número de 
+ * elementos do vetor **n**
+ * 
+ * @param N Representa o tamanho do módulo 
+ * @param vect Representa o módulo
+ * @return Retorna o valor da média calculada
+ * 
+ * @verbatim
+   Autor:
+    * Guilherme Cajeira, nº93138
+    * Email: gcajeira@ua.pt @endverbatim
+ */
+    int MySAGAvg(int N,int *stream);
+
+/**
+ * @brief Verificar o número de vezes que um certo valor foi inserido no módulo **stream**
+ * 
+ * A função MySAGMax percorre todo o módulo e identifica o número de ocorrẽncias de um dado valor.
+ * O contador é incrementado cada vez que uma ocorrência é observada.
+ * 
+ * @param N Representa o tamanho do módulo
+ * @param stream Representa o módulo
+ * @return Retorna o número de vezes que um certo valor aparece no módulo
+ * 
+ * @verbatim
+   Autor:
+    * Dário Fernandes, nº93135
+    * Email: d.fernandes15@ua.pt @endverbatim
+ */
 int MySAGFreq(int N,int *stream,int x);
 
 #endif
