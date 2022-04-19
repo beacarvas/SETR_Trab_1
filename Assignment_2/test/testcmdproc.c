c359a5e08ee5ed0e5ced1de9773f2a4837d0a181
Dário Fernandes <dario@pop-os.localdomain>
2022-04-19 10:30:45 +0100

    Update

--- Assignment_2/test/testcmdproc.c
+++ Assignment_2/test/testcmdproc.c
@@ -14,12 +14,30 @@
 
 extern char Kp, Ti, Td;
 
+
+void setUp(void)
+{
+	return;
+}
+void tearDown(void)
+{
+	return;
+}
+
+void test_res_1(void)
+{
+	TEST_ASSERT_EQUAL_INT(0, cmdProcessor());
+
+}
 int main(void) 
 {
+	UNITY_BEGIN();
+
 	int res;
 	
 	printf("Command processor test app\n\r");
 	resetCmdString();
+	newCmdChar('*');
 	newCmdChar('#');
 	newCmdChar('P');
 	newCmdChar('1');
@@ -27,19 +45,29 @@ int main(void)
 	newCmdChar('3');
 	newCmdChar((unsigned char)('P'+'1'+'2'+'3'));
 	newCmdChar('!');
-	res=cmdProcessor();
+	/* res=cmdProcessor(); */
+	RUN_TEST(test_res_1);
 	printf("cmdProcessor output to P 1 2 3: %d, Kp=%c,Ti=%c,Td=%c \n\r", res, Kp, Ti, Td);
 	
+	resetCmdString();
 	newCmdChar('#');
 	newCmdChar('D');
 	newCmdChar('!');
-	res=cmdProcessor();
+	/* res=cmdProcessor(); */
+	RUN_TEST(test_res_1);
 	printf("cmdProcessor output to D (typo, should be S): % d\n\r", res);
 	
+
+	resetCmdString();
 	newCmdChar('+');
 	newCmdChar('S');
 	newCmdChar('!');
-	res=cmdProcessor();
+	newCmdChar('#');
+	/* newCmdChar('P'); */
+	newCmdChar('!');
+	/* res=cmdProcessor(); */
+	RUN_TEST(test_res_1);
 	printf("cmdProcessor output to S with wrong SOF: % d\n\r", res);
 	
+	return UNITY_END();
 }
